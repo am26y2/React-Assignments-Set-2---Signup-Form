7623d7f5b84b4ffd3428f10b53ff73e63e76ecdf
am26y2 <amitraj9046@gmail.com>
2020-12-19 16:39:54 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,118 +1,143 @@
-import React, { useState } from "react";
-//import "../styles/App.css";
+import React from "react";
+import "../styles/App.css";
 
-const App = () => {
-  //const [name, setName] = useState("");
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      gender: "male",
+      phNo: "",
+      password: "",
+      errorMessage: "",
+      userName: ""
+    };
+  }
 
-  const [inputFields, setInputFields] = useState({
-    name: "",
-    emailId: "",
-    gender: "male",
-    phoneNumber: "",
-    password: "",
-    userName: ""
-  });
-  const [errorFields, setErrorFields] = useState("");
-  const handleChange = (event) => {
-    let name = event.target.name;
-    let value = event.target.value;
-    setInputFields((prevInputFields) => {
-      return { ...prevInputFields, [name]: value };
+  handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePhoneNoChange = (event) => {
+    this.setState({ phNo: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleChangeValue = (event) => {
+    this.setState({ gender: event.target.value });
+  };
+
+  handleSubmit = () => {
+    const alphanumeric = /^[0-9a-zA-Z ]+$/;
+    const numbers = /^\d+$/;
+    if (
+      this.state.name === "" ||
+      this.state.email === "" ||
+      this.state.phNo === "" ||
+      this.state.gender === "" ||
+      this.state.password === ""
+    ) {
+      this.setState({ errorMessage: "All fields are mandatory", userName: "" });
+      return;
+    }
+    if (!this.state.name.match(alphanumeric)) {
+      this.setState({ errorMessage: "Name is not alphanumeric", userName: "" });
+      return;
+    }
+    if (this.state.email.indexOf("@") < 1) {
+      this.setState({ errorMessage: "Email must contain @", userName: "" });
+      return;
+    }
+
+    if (!this.state.gender) {
+      this.setState({
+        errorMessage: "Please identify as male, female or others",
+        userName: ""
+      });
+      return;
+    }
+    if (!numbers.test(this.state.phNo)) {
+      this.setState({
+        errorMessage: "Phone Number must contain only numbers",
+        userName: ""
+      });
+      return;
+    }
+    if (this.state.password.length < 6) {
+      this.setState({
+        errorMessage: "Password must contain atleast 6 letters",
+        userName: ""
+      });
+      return;
+    }
+    const user = this.state.email.substring(0, this.state.email.indexOf("@"));
+    this.setState({
+      userName: user,
+      errorMessage: "",
+      name: "",
+      email: "",
+      gender: "male",
+      phNo: "",
+      password: ""
     });
   };
-  const handleClick = () => {
-    const alphanumeric = /^[0-9a-zA-Z]+$/;
-    const number = /^\d+$/;
-    if (
-      inputFields.name === "" ||
-      inputFields.emailId === "" ||
-      inputFields.gender === "" ||
-      inputFields.phoneNumber === "" ||
-      inputFields.password === ""
-    ) {
-      setErrorFields("All fields are mandatory");
-    } else if (inputFields.name===null || inputFields.name.match(alphanumeric)) {
-      setErrorFields("Name is not alphanumeri");
-    } else if (inputFields.emailId===null || inputFields.emailId.includes("@") === false) {
-      setErrorFields("Email must contain @");
-    } else if (
-      inputFields.gender !== "male" &&
-      inputFields.gender !== "female" &&
-      inputFields.gender !== "other"
-    ) {
-      setErrorFields("Please identify as male, female or others");
-    } else if (inputFields.phoneNumber===null || !isNan(inputFields.phoneNumbe)) {
-      setErrorFields("Phone Number must contain only numbers");
-    } else if (inputFields.password.length < 6) {
-      setErrorFields("Password must contain atleast 6 letters");
-    } else {
-      let index = inputFields.emailId.indexOf("@");
-      let name = inputFields.emailId.substring(0, index);
-      setInputFields((prevInputFields) => {
-        return { ...prevInputFields, userName: name };
-      });
-      setErrorFields(true);
-    }
-  };
 
-  return (
-    <div id="main">
-      {errorFields === true ? (
-        <div>{`Hello ${inputFields.userName}`}</div>
-      ) : (
-        <div>{errorFields}</div>
-      )}
-      <div>
+  render() {
+    return (
+      <>
+        {this.state.errorMessage && <div>{this.state.errorMessage}</div>}
+        {this.state.userName && <div>Hello {this.state.userName}</div>}
         <input
           data-testid="name"
           type="text"
           name="name"
-          placeholder="Enter Your Name"
-          value={inputFields.name}
-          onChange={handleChange}
-        ></input>
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
         <input
           data-testid="email"
           type="text"
-          name="emailId"
-          placeholder="Enter Your Email Id"
-          value={inputFields.emailID}
-          onChange={handleChange}
-        ></input>
+          name="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
         <input
           data-testid="gender"
           type="text"
           name="gender"
-          placeholder="Enter Your Gender (male or female or other)"
-          value={inputFields.gender}
-          defaultValue="male"
-          onChange={handleChange}
-        ></input>
+          value={this.state.gender}
+          onChange={this.handleChangeValue}
+        />
         <input
           data-testid="phoneNumber"
-          type="number"
+          type="text"
           name="phoneNumber"
-          placeholder="Enter Your Phone Number"
-          value={inputFields.phoneNumber}
-          onChange={handleChange}
-        ></input>
+          placeholder="Phone Number"
+          value={this.state.phNo}
+          onChange={this.handlePhoneNoChange}
+        />
         <input
           data-testid="password"
           type="password"
           name="password"
-          placeholder="Enter Your Password"
-          value={inputFields.password}
-          onChange={handleChange}
-        ></input>
-        <button data-testid="submit" onClick={handleClick}>
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <button data-testid="submit" onClick={this.handleSubmit}>
           Submit
         </button>
-      </div>
-    </div>
-  );
-};
-
-export default App;
-function newFunction() {
-  return "true";
+      </>
+    );
+  }
 }
